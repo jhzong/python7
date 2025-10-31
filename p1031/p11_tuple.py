589879f913ca1c2588062c99a5d4b72d0024ac39
jhzong <jhzong05@gmail.com>
2025-10-31 18:24:39 +0900

    수업

---
+++ p1031/p11_tuple.py
@@ -0,0 +1,18 @@
+# 튜플 - 리스트와 동일, 수정 불가
+
+a_list=[1,2,3,4,5]
+a_tu=(1,2,3,4,5)
+print(a_list)
+print(a_tu)
+print(a_list[2])
+print(a_tu[2]) # []로 안하면 error
+a_list[0]=100
+print(a_list)
+# a_tu[0]=100 # 튜플 수정불가
+# print(a_tu)
+
+del(a_list[0])
+print(a_list)
+
+# del(a_tu[0]) # 튜플 삭제불가
+# print(a_tu)
